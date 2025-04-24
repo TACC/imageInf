cab3b08a33b67e474c2c20b7c819185a6e4e8515
Nathan Franklin <nathan.franklin@gmail.com>
2025-04-24 10:21:29 -0500

    Rename status.py to status_routes.py

--- imageinf/main.py
+++ imageinf/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from imageinf import status
+from imageinf import status_routes
 
 app = FastAPI(
     title="imageInf API",
     description="A simple image inference API",
     version="0.1.0",
 )
-app.include_router(status.router)
+app.include_router(status_routes.router)
