ca75c57fd7b2904408a6a7d53468d67b219286d9
Nathan Franklin <nathan.franklin@gmail.com>
2025-12-18 11:40:36 -0600

    Hardcode dev.cep as work-around

--- client/src/hooks/useToken.ts
+++ client/src/hooks/useToken.ts
@@ -38,7 +38,8 @@ const fetchTokenFromPortal = async (): Promise<string | null> => {
   try {
     // Since we're on the same domain (via an iframe), use relative path
     // The cookie will be sent automatically with credentials: 'include'
-    const response = await fetch('/api/auth/tapis/', {
+    // TODO determine host CEP not have it hardcoded
+    const response = await fetch('https://dev.cep.tacc.utexas.edu/api/auth/tapis/', {
       credentials: 'include',
     });
 
