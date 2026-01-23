506194cb1ec8b481c094837f9a29aa33e9146ce9
Nathan Franklin <nathan.franklin@gmail.com>
2026-01-23 17:12:41 -0600

    Add classify demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import MainPage from './pages/MainPage';
+import DemoPage from './pages/DemoPage';
 import Login from './pages/Login';
 import Logout from './pages/Logout';
 import Callback from './pages/Callback';
@@ -14,6 +15,7 @@ function App() {
       <BrowserRouter basename="/imageinf/ui">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/cep-demo" element={<DemoPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
           <Route path="/auth/callback" element={<Callback />} />
